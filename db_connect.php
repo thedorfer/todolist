e0d9bf87c176c0afda3c9d3625eff018b17246f8
thedorfer <thedorfer@gmail.com>
2018-10-04 22:11:43 -0500

    Update db_connect and add setup files
    changes include the setup files to initialize the database for new installs.

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,23 @@
 <?php
 
-function db(){
+function mysqlInfo(){
+	//Edit values to match mysql server info
 	$mysql_host = 'localhost';
 	$mysql_user = 'root';
 	$mysql_pw = '';
+	
+	$dbinfo = array($mysql_host, $mysql_user, $mysql_pw);
+	return($dbinfo);
+}
+
+function db(){
+	
+	$dbinfo = mysqlInfo();
+	
+	$mysql_host = $dbinfo[0];
+	$mysql_user = $dbinfo[1];
+	$mysql_pw = $dbinfo[2];
+	
 	$mysql_db = 'todolist';
 	
 	global $link;
@@ -11,6 +25,39 @@ function db(){
     $link = mysqli_connect($mysql_host, $mysql_user, $mysql_pw, $mysql_db) or die('connection failed');
 	
 	return $link;
+}
+
+function initDB(){
+	
+	$dbinfo = mysqlInfo();
+	
+	$mysql_host = $dbinfo[0];
+	$mysql_user = $dbinfo[1];
+	$mysql_pw = $dbinfo[2];
+	
+	// Create connection
+	$link = new mysqli($mysql_host, $mysql_user, $mysql_pw);
+	// Check connection
+	if ($link->connect_error) {
+		die("Connection failed: " . $link->connect_error);
+	}
+
+	// Create database
+	$query = "CREATE DATABASE todolist";
+	if ($link->query($query) === TRUE) {
+		echo "Database created successfully";
+	} else {
+		echo "Error creating database: " . $link->error;
+	}
+
+	$query = "CREATE TABLE `todolist`.`todos` ( `id` INT NOT NULL AUTO_INCREMENT , `todoDescription` TEXT NOT NULL , `date` DATETIME NOT NULL , PRIMARY KEY (`id`));";
+	if ($link->query($query) === TRUE) {
+		echo "Database created successfully";
+	} else {
+		echo "Error creating database: " . $link->error;
+	}
+
+	mysqli_close($link);	
 }
 
 ?>
---
+++ setup.php
@@ -0,0 +1,8 @@
+
+<?php
+require_once('db_connect.php');
+
+initDB();
+
+header("Refresh:0; url=index.php");
+?> 
